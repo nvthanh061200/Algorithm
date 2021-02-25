6c8577776ba986515fbd44edab407f6fde524bb3
Nguyen Van Thanh <nvthanh061200@gmail.com>
2021-02-25 13:04:12 +0700

    Quick sort

--- Quick_Sort.js
+++ Quick_Sort.js
@@ -1,26 +1,43 @@
-// Nguyễn Văn THành - KTPM - K17A
+var items = [5, 3, 6, 2, 1];
+function swap(items, leftIndex, rightIndex) {
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    let pivot = items[Math.floor((right + left) / 2)]//middle element
+    let i = left //left pointer
+    let j = right; //right pointer
+    console.log(pivot);
 
-console.time();
-const quickSort = (arr) => {
-
-    if(arr.length < 2) return arr;
-
-    const pivotIndex = arr.length - 1; // lấy phần tử cuối arr làm pivot
-    const pivot = arr[pivotIndex];
-
-    const left = [];
-    const right = [];
-    
-    for(let i = 0; i < pivotIndex; i++) {
-        if(arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
         }
     }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
-
+    return i;
 }
-console.log(quickSort([10,3,13,2,0,9]));
-console.timeEnd();
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+var sortedArray = quickSort(items, 0, items.length - 1);
+console.log(sortedArray);
